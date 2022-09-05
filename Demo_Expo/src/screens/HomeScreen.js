a94da7b58b411f70f2756783f4c26729538b0893
tinhvan <huynhvantinhftu@gmail.com>
2022-09-05 19:55:01 +0700

    MACOS

--- Demo_Expo/src/screens/HomeScreen.js
+++ Demo_Expo/src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen. Xin Chào Bạn!</Text>;
+  return <Text style={styles.text}>HomeScreen. Chào Bạn đến với ứng dụng học tiếng Anh TOBE!</Text>;
 };
 
 const styles = StyleSheet.create({
